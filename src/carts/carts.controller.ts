183571e5aba4ad46e68e27b6e43d0b22c762e74a
Ahmad Tamer <alient2001@outlook.com>
2024-07-23 06:26:28 +0300

    feat! : Add deleteItemFromCart endpoint

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Headers, UseGuards, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+  Headers,
+  UseGuards,
+  Put,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CartsService } from './carts.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/updateCartRequest';
@@ -7,39 +20,64 @@ import { AuthGuard } from 'src/users/auth.guard';
 import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 import { AuthUser } from 'src/users/user.decorator';
 import { CartItemDto } from './dto/cartItem.dto';
+import { DeleteItemFromCartDto } from './dto/deleteItemFromCart.dto';
+import { UpdateCartItemQuantityDto } from './dto/updateCartItemQuantity.dto';
 
 @Controller('api/carts')
 @ApiBearerAuth('bearer')
 export class CartsController {
   constructor(private readonly cartsService: CartsService) {}
 
-  @ApiBody({type: AddToCartRequest})
+  @ApiBody({ type: AddToCartRequest })
   @UseGuards(AuthGuard)
   @Post('add')
-  async addToCart(@Body() addToCartDto: AddToCartRequest, @AuthUser() user: any) {
-    const cartDto = new CartItemDto();
-    cartDto.userId = user.userId;
-    cartDto.productId = addToCartDto.productId;
-    cartDto.quantity = addToCartDto.quantity;
-    return this.cartsService.addToCart(cartDto);
+  async addToCart(
+    @Body() addToCartDto: AddToCartRequest,
+    @AuthUser() user: any,
+  ) {
+    return this.cartsService.addToCart(
+      user.userId,
+      addToCartDto.productId,
+      addToCartDto.quantity,
+    );
   }
 
   @UseGuards(AuthGuard)
   @Get(':userId')
-  findCartItemsByUserId(@Param('userId',ParseIntPipe) id: number) {
+  findCartItemsByUserId(
+    @Param('userId', ParseIntPipe) id: number,
+    @AuthUser() user: any,
+  ) {
+    if (user?.userId !== id) {
+      throw new ForbiddenException();
+    }
     return this.cartsService.findCartItemsByUserId(+id);
   }
 
-  @Put("update")
+  @Put('update')
   @UseGuards(AuthGuard)
-  @ApiBody({type: AddToCartRequest})
-  updateCartItemQuantity(@Body() updateCartDto: AddToCartRequest, @AuthUser() user: any){
-    const cartDto = new CartItemDto();
-    cartDto.userId = user.userId;
-    cartDto.productId = updateCartDto.productId;
-    cartDto.quantity = updateCartDto.quantity;
-    return this.cartsService.update(cartDto);
+  @ApiBody({ type: UpdateCartItemQuantityDto })
+  updateCartItemQuantity(
+    @Body() updateCartDto: UpdateCartItemQuantityDto,
+    @AuthUser() user: any,
+  ) {
+    return this.cartsService.update(
+      user.userId,
+      updateCartDto.productId,
+      updateCartDto.quantity,
+    );
   }
 
-
+  @Delete('remove')
+  @UseGuards(AuthGuard)
+  @ApiBody({ type: DeleteItemFromCartDto })
+  removeCartItem(
+    @Body() deleteItemFromCartDto: DeleteItemFromCartDto,
+    @AuthUser() user: any,
+  ) {
+    return this.cartsService.deleteItemFromCart(
+      user.userId,
+      deleteItemFromCartDto.productId,
+    );
+  }
 }
--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -1,31 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/updateCartRequest';
 import { CartsRepository } from './carts.repository';
 import { AddToCartRequest } from './dto/addToCartRequest.dto';
 import { CartItemDto } from './dto/cartItem.dto';
+import { UsersService } from 'src/users/users.service';
+import { ProductsService } from 'src/products/products.service';
 
 @Injectable()
 export class CartsService {
-  constructor(private repo: CartsRepository){}
+  constructor(
+    private repo: CartsRepository,
+    private userService: UsersService,
+    private productService: ProductsService,
+  ) {}
 
-  async addToCart(cartDto: CartItemDto) {
-    return this.repo.addToCart(cartDto);
+  async addToCart(userId: number, productId: number, quantity: number) {
+    return await this.repo.addToCart(userId, productId, quantity);
   }
 
   findAll() {
     return `This action returns all carts`;
   }
+  async isProductInUserCart(userId: number, productId: number) {
+    const user = await this.userService.getUserById(userId);
+    const product = await this.productService.getProductById(productId);
 
+    return await this.repo.isProductInUserCart(userId, productId);
+  }
   async findCartItemsByUserId(id: number) {
     return await this.repo.getCartItemsByUserId(id);
   }
 
-  update(updateCartDto: CartItemDto) {
-    return this.repo.updateCart(updateCartDto);
+  async update(userId: number, productId: number, quantity: number) {
+    if (!(await this.isProductInUserCart(userId, productId)))
+      throw new BadRequestException(
+        "Product does not exist in the current user's cart.",
+      );
+
+    if (quantity == 0) {
+      return await this.deleteItemFromCart(userId, productId);
+    }
+
+    return await this.repo.updateCart(userId, productId, quantity);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cart`;
+  async deleteItemFromCart(userId: number, productId: number) {
+    if (!(await this.isProductInUserCart(userId, productId)))
+      throw new BadRequestException(
+        "Product does not exist in the current user's cart.",
+      );
+
+    return await this.repo.deleteItemFromCart(userId, productId);
   }
 }
