69e6cb74422a3af1e5d804fcfa94ea4b730bedd9
Ahmad Tamer <alient2001@outlook.com>
2024-07-23 17:44:08 +0300

    fix: Swagger CSS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
+
 const server = express();
 
 async function bootstrap(expressInstance) {
@@ -32,7 +33,16 @@ async function bootstrap(expressInstance) {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('', app, document, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.init();
 }
