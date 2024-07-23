ee9d6d3b20eb01fac5aefb4598661e07b418cc6e
Ahmad Tamer <alient2001@outlook.com>
2024-07-23 08:02:04 +0300

    feat! : Add CreateOrder Endpoint

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,16 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { AuthUser } from 'src/users/user.decorator';
+import { AuthGuard } from 'src/users/auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('orders')
+@ApiBearerAuth('bearer')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  // @Post()
-  // create(@Body() createOrderDto: CreateOrderDto) {
-  //   return this.ordersService.create(createOrderDto);
-  // }
+  @Post()
+  @UseGuards(AuthGuard)
+  create(@AuthUser() user: any) {
+    return this.ordersService.createOrder(user.userId);
+  }
 
   // @Get()
   // findAll() {
