9b54924d9d7fbd82a4a8896466babcdf76d740aa
Ahmad Tamer <alient2001@outlook.com>
2024-07-23 08:01:21 +0300

    chore: Adjust OrdersModule

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { OrdersRepository } from './orders.repository';
+import { CartsService } from 'src/carts/carts.service';
+import { CartsRepository } from 'src/carts/carts.repository';
+import { CartsModule } from 'src/carts/carts.module';
+import { ProductsService } from 'src/products/products.service';
+import { ProductsRepository } from 'src/products/products.repository';
+import { UsersService } from 'src/users/users.service';
+import { UsersRepository } from 'src/users/users.repository';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [
+    OrdersService,
+    OrdersRepository,
+    CartsService,
+    CartsRepository,
+    ProductsService,
+    ProductsRepository,
+    UsersService,
+    UsersRepository
+  ],
+  imports: [PrismaModule, CartsModule],
 })
 export class OrdersModule {}
